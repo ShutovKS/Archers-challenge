95700f130f7fab8cdc278680f1cc01d9889ab9f8
ShutovKS <schutow.kir@yandex.ru>
2024-07-01 12:20:48 +0300

    Add ProjectStateMachine

---
+++ src/Archery at targets/Assets/!Project Files/Scripts/ProjectStateMachine/GameBootstrap.cs
@@ -0,0 +1,20 @@
+﻿using ProjectStateMachine.Base;
+using ProjectStateMachine.States;
+using UnityEngine;
+
+namespace ProjectStateMachine
+{
+    public class GameBootstrap : MonoBehaviour
+    {
+        private void Start()
+        {
+            StateMachine = new StateMachine<GameBootstrap>(new BootstrapState(this),
+                new GameMainMenuState(this)
+            );
+
+            StateMachine.SwitchState<BootstrapState>();
+        }
+
+        public StateMachine<GameBootstrap> StateMachine;
+    }
+}
---
+++ src/Archery at targets/Assets/!Project Files/Scripts/ProjectStateMachine/States/GameMainMenuState.cs
@@ -0,0 +1,54 @@
+﻿using ProjectStateMachine.Base;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectStateMachine.States
+{
+    public class GameMainMenuState : IState<GameBootstrap>, IEnterable, IExitable
+    {
+        public GameBootstrap Initializer { get; }
+
+        public GameMainMenuState(GameBootstrap initializer)
+        {
+            Initializer = initializer;
+        }
+
+        public void OnEnter()
+        {
+            var loadSceneAsync = SceneManager.LoadSceneAsync("MainMenu");
+            loadSceneAsync!.completed += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(AsyncOperation asyncOperation)
+        {
+            var mainMenuUI = Object.FindFirstObjectByType<MainMenuUI>();
+            mainMenuUI.OnVrGameButtonClicked += OnVrGameButtonClicked;
+            mainMenuUI.OnMrGameButtonClicked += OnMrGameButtonClicked;
+            mainMenuUI.OnExitButtonClicked += OnExitButtonClicked;
+        }
+
+        public void OnExit()
+        {
+        }
+
+        private void OnVrGameButtonClicked()
+        {
+            // Initializer.StateMachine.SwitchState<GameVrGameState>();
+        }
+
+        private void OnMrGameButtonClicked()
+        {
+            // Initializer.StateMachine.SwitchState<GameMrGameState>();
+        }
+
+        private void OnExitButtonClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
