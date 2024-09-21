60b64a5cd193a744b418bf5a83117674f947aae1
ShutovKS <schutow.kir@yandex.ru>
2024-09-21 13:01:54 +0300

    Update RandomPositionsOnARPlane

--- src/Archery at targets/Assets/!Project Files/Code/Scripts/Features/PositionsContainer/RandomPositionsOnARPlane.cs
+++ src/Archery at targets/Assets/!Project Files/Code/Scripts/Features/PositionsContainer/RandomPositionsOnARPlane.cs
@@ -96,19 +96,38 @@ namespace Features.PositionsContainer
         private Vector3 GetRandomPointOnPlane(ARPlane plane)
         {
             var boundary = plane.boundary;
-            if (boundary.Length == 0)
-            {
-                throw new InvalidOperationException("Selected AR plane has no valid boundary.");
-            }
 
-            return plane.transform.TransformPoint(boundary[UnityEngine.Random.Range(0, boundary.Length)]);
+            if (boundary.Length == 0) throw new InvalidOperationException("Selected AR plane has no valid boundary.");
+
+            const int MAX_ATTEMPTS = 10;
+            var attempts = 0;
+
+            do
+            {
+                var randomPoint =
+                    plane.transform.TransformPoint(boundary[UnityEngine.Random.Range(0, boundary.Length)]);
+                attempts++;
+
+                var cameraPosition = _cameraService.CameraPosition;
+                var direction = randomPoint - cameraPosition;
+                var distance = direction.magnitude;
+                var mask = LayerMask.GetMask("Target", "Arrow", "Interactable", "Player", "UI", "Interactable Head");
+
+                if (!Physics.Raycast(cameraPosition, direction.normalized, distance, mask))
+                {
+                    return randomPoint;
+                }
+            } while (attempts < MAX_ATTEMPTS);
+
+            return Vector3.zero;
         }
+
 
         private Quaternion GetRotation(Vector3 position)
         {
             var cameraPosition = _cameraService.CameraPosition;
             var direction = position - cameraPosition;
-            return Quaternion.LookRotation(direction);
+            return Quaternion.LookRotation(direction, Vector3.up);
         }
     }
 }
