d972ae38df1b5396ccddcc262ab8707e874fa461
ShutovKS <schutow.kir@yandex.ru>
2025-01-05 23:21:34 +0300

    fix ARPlaneColorizer

---
+++ src/Archers challenge/Assets/!Project Files/Code/Scripts/Features/SurfaceRendering/ARBoundingBoxColorizer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace Features.SurfaceRendering
+{
+    [RequireComponent(typeof(ARBoundingBox))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class ARBoundingBoxColorizer : MonoBehaviour
+    {
+        private ARBoundingBox _arBoundingBox;
+        private MeshRenderer _meshRenderer;
+
+        private void Awake()
+        {
+            _arBoundingBox = GetComponent<ARBoundingBox>();
+            _meshRenderer = GetComponent<MeshRenderer>();
+
+            UpdatePlaneColor();
+        }
+
+        private void UpdatePlaneColor()
+        {
+            var color = GetColorByClassification(_arBoundingBox.classifications);
+            color.a = 0.25f;
+            _meshRenderer.material.color = color;
+        }
+
+        private static Color GetColorByClassification(BoundingBoxClassifications type) => type switch
+        {
+            BoundingBoxClassifications.None          => new Color(1f, 1f, 1f),
+            BoundingBoxClassifications.Couch         => new Color(0.5f, 0.2f, 0.2f),
+            BoundingBoxClassifications.Table         => new Color(0.2f, 0.6f, 0.5f),
+            BoundingBoxClassifications.Bed           => new Color(0.6f, 0.4f, 0.7f),
+            BoundingBoxClassifications.Lamp          => new Color(1f, 0.9f, 0.6f),
+            BoundingBoxClassifications.Plant         => new Color(0.2f, 0.8f, 0.3f),
+            BoundingBoxClassifications.Screen        => new Color(0.1f, 0.3f, 0.7f),
+            BoundingBoxClassifications.Storage       => new Color(0.5f, 0.4f, 0.3f),
+            BoundingBoxClassifications.Bathtub       => new Color(0.7f, 0.9f, 1f),
+            BoundingBoxClassifications.Chair         => new Color(0.7f, 0.5f, 0.3f),
+            BoundingBoxClassifications.Dishwasher    => new Color(0.6f, 0.6f, 0.8f),
+            BoundingBoxClassifications.Fireplace     => new Color(0.9f, 0.4f, 0.2f),
+            BoundingBoxClassifications.Oven          => new Color(0.8f, 0.7f, 0.6f),
+            BoundingBoxClassifications.Refrigerator  => new Color(0.7f, 0.8f, 0.9f),
+            BoundingBoxClassifications.Sink          => new Color(0.8f, 0.9f, 1f),
+            BoundingBoxClassifications.Stairs        => new Color(0.4f, 0.3f, 0.2f),
+            BoundingBoxClassifications.Stove         => new Color(0.7f, 0.7f, 0.7f),
+            BoundingBoxClassifications.Toilet        => new Color(0.9f, 0.9f, 0.9f),
+            BoundingBoxClassifications.WasherDryer   => new Color(0.8f, 0.8f, 0.9f),
+            BoundingBoxClassifications.Other         => new Color(0.5f, 0.5f, 0.5f),
+            _                                        => new Color(1f, 1f, 1f)
+        };
+    }
+}
--- src/Archers challenge/Assets/!Project Files/Code/Scripts/Features/SurfaceRendering/ARPlaneColorizer.cs
+++ src/Archers challenge/Assets/!Project Files/Code/Scripts/Features/SurfaceRendering/ARPlaneColorizer.cs
@@ -9,36 +9,39 @@ namespace Features.SurfaceRendering
     public class ARPlaneColorizer : MonoBehaviour
     {
         private ARPlane _arPlane;
-        private MeshRenderer _planeMeshRenderer;
+        private MeshRenderer _meshRenderer;
 
         private void Awake()
         {
             _arPlane = GetComponent<ARPlane>();
-            _planeMeshRenderer = GetComponent<MeshRenderer>();
+            _meshRenderer = GetComponent<MeshRenderer>();
 
             UpdatePlaneColor();
         }
 
         private void UpdatePlaneColor()
         {
-            var planeMatColor = GetColorByClassification(_arPlane.classification);
-
-            planeMatColor.a = 0.0f;
-            _planeMeshRenderer.material.color = planeMatColor;
+            var color = GetColorByClassification(_arPlane.classifications);
+            color.a = 0.25f;
+            _meshRenderer.material.color = color;
         }
 
-        private static Color GetColorByClassification(PlaneClassification classifications) => classifications switch
+        private static Color GetColorByClassification(PlaneClassifications type) => type switch
         {
-            PlaneClassification.None => Color.green,
-            PlaneClassification.Wall => Color.white,
-            PlaneClassification.Floor => Color.red,
-            PlaneClassification.Ceiling => Color.yellow,
-            PlaneClassification.Table => Color.blue,
-            PlaneClassification.Seat => Color.blue,
-            PlaneClassification.Door => Color.blue,
-            PlaneClassification.Window => new Color(1f, 0.4f, 0f), //orange
-            PlaneClassification.Other => Color.magenta,
-            _ => Color.gray
+            PlaneClassifications.None             => new Color(1f, 1f, 1f),
+            PlaneClassifications.Ceiling          => new Color(0.7f, 0.7f, 1f),
+            PlaneClassifications.DoorFrame        => new Color(0.8f, 0.4f, 0.2f),
+            PlaneClassifications.Floor            => new Color(0.4f, 0.3f, 0.3f),
+            PlaneClassifications.WallArt          => new Color(0.9f, 0.5f, 0.5f),
+            PlaneClassifications.WallFace         => new Color(0.6f, 0.4f, 0.3f),
+            PlaneClassifications.WindowFrame      => new Color(0.8f, 0.8f, 0.2f),
+            PlaneClassifications.Couch            => new Color(0.5f, 0.2f, 0.2f),
+            PlaneClassifications.Seat             => new Color(0.5f, 0.2f, 0.5f),
+            PlaneClassifications.SeatOfAnyType    => new Color(0.7f, 0.3f, 0.7f),
+            PlaneClassifications.Table            => new Color(0.2f, 0.6f, 0.5f),
+            PlaneClassifications.InvisibleWallFace=> new Color(0.7f, 0.7f, 0.7f),
+            PlaneClassifications.Other            => new Color(0.5f, 0.5f, 0.5f),
+            _                                     => new Color(1f, 1f, 1f)
         };
     }
 }
